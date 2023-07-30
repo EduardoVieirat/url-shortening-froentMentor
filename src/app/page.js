fe12383cd170d3261d94ca5abdcb3164d3f215de
EduardoOdev <duduvieiratou@outlook.com>
2023-07-30 13:53:53 -0300

    adicionando página principal

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@
 import Header from '@/components/headerBar/header'
 import './style.css'
 import MainSection from '@/components/mainSection/main'
+import Shortner from '@/components/shortnerUrlsection/shortner'
 
 export default function Home() {
   return (
     <main className='main'>
       <Header/>
       <MainSection/>
+      <Shortner/>
     </main>
   )
 }
