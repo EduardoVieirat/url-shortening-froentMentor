3cb75f67c2846802792ca0718ec123c5efcdad38
EduardoOdev <duduvieiratou@outlook.com>
2023-07-30 18:05:48 -0300

    adicionando estado de mensagem de erro

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ import api from "@/api/api";
 
 export default function Home() {
   const [urlToShort, setUrlToShort] = useState("");
-  
+  const [errorMessage, setErrorMessage] = useState('')
+
   async function HandleUrlShortner() {
 
     try {
@@ -18,16 +19,16 @@ export default function Home() {
     } catch (error) {
       const { error_code } = error.response.data;
 
-      error_code == 1 && setUrlToShort('Parâmetro da Url está vazio');
-      error_code == 2 && setUrlToShort('Url enviado é inválido');
-      error_code == 3 && setUrlToShort('Espere um segundo e tente novamente');
-      error_code == 4 && setUrlToShort('O endereço IP foi bloqueado por violar nossos termos de serviço');
-      error_code == 5 && setUrlToShort('código shrtcode (slug) já usado/em uso');
-      error_code == 6 || error_code > 10 && setUrlToShort('Erro desconhecido');
-      error_code == 7 && setUrlToShort('Nenhum código especificado (o parâmetro "código" está vazio)');
-      error_code == 8 && setUrlToShort('Código inválido enviado (código não encontrado/não existe esse link curto)');
-      error_code == 9 && setUrlToShort('Parâmetros obrigatórios ausentes');
-      error_code == 10 && setUrlToShort('Tentando encurtar um link não permitido');
+      error_code == 1 && setErrorMessage('Parâmetro da Url está vazio');
+      error_code == 2 && setErrorMessage('Url enviado é inválido');
+      error_code == 3 && setErrorMessage('Espere um segundo e tente novamente');
+      error_code == 4 && setErrorMessage('O endereço IP foi bloqueado por violar nossos termos de serviço');
+      error_code == 5 && setErrorMessage('código shrtcode (slug) já usado/em uso');
+      error_code == 6 || error_code > 10 && setErrorMessage('Erro desconhecido');
+      error_code == 7 && setErrorMessage('Nenhum código especificado (o parâmetro "código" está vazio)');
+      error_code == 8 && setErrorMessage('Código inválido enviado (código não encontrado/não existe esse link curto)');
+      error_code == 9 && setErrorMessage('Parâmetros obrigatórios ausentes');
+      error_code == 10 && setErrorMessage('Tentando encurtar um link não permitido');
 
     }
   }
@@ -40,6 +41,7 @@ export default function Home() {
         urlToShort={urlToShort}
         setUrlToShort={setUrlToShort}
         HandleUrlShortner={HandleUrlShortner}
+        errorMessage={errorMessage}
       />
     </main>
   );
