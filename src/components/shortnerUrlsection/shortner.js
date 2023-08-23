7b6734b88e6a27e196ad133f8d3e4f0570cddbcf
EduardoOdev <duduvieiratou@outlook.com>
2023-08-23 20:03:06 -0300

    feat: add a function to short the url when press 'Enter'

--- src/components/shortnerUrlsection/shortner.js
+++ src/components/shortnerUrlsection/shortner.js
@@ -1,13 +1,34 @@
+import { UrlContext } from "@/context/context";
 import "./style.css";
+import { useContext } from "react";
 
-export default function Shortner({
-  urlToShort,
-  setUrlToShort,
-  HandleUrlShortner,
-  errorMessage
-}) {
+export default function Shortner({ HandleUrlShortner, errorMessage }) {
+  const { urlToShort, setUrlToShort, setUrlShorted } = useContext(UrlContext);
+
   function handleInput(event) {
     setUrlToShort(event.target.value);
+  }
+
+  function handleEnter(event) {
+    if (event.key === "Enter") {
+      if (event.target.value == "") {
+        return setUrlShorted({
+          code: "",
+          short_link: "",
+          full_short_link: "",
+          short_link2: "",
+          full_short_link2: "",
+          short_link3: "",
+          full_short_link3: "",
+          share_link: "",
+          full_share_link: "",
+          original_link: "",
+        });
+      }
+
+      HandleUrlShortner();
+      setUrlToShort(event.target.value);
+    }
   }
 
   return (
@@ -15,18 +36,20 @@ export default function Shortner({
       <div>
         <input
           onChange={handleInput}
-          className={`input-shortner ${errorMessage !== "" && 'error-alert'}`}
+          className={`input-shortner ${errorMessage !== "" && "error-alert"}`}
           type="text"
           placeholder="Shorten a link here..."
           value={urlToShort}
-          />
-          {errorMessage !== '' && <span className="error-message">{errorMessage}</span>}
-        </div>
+          onKeyDown={handleEnter}
+        />
+        {errorMessage !== "" && (
+          <span className="error-message">{errorMessage}</span>
+        )}
+      </div>
 
       <button className="btn-shortner" onClick={() => HandleUrlShortner()}>
         Shorten It!
       </button>
-
     </section>
   );
 }
