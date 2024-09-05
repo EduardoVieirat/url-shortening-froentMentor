a78ab98327519ac35164b0c9d8f206722ff2990d
EduardoOdev <duduvieiratou@outlook.com>
2024-09-04 21:51:04 -0300

    [feat]: using a new API

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.shrtco.de/v2",
+  baseURL: "https://spoo.me",
   timeout: 10000,
+  headers: {
+    "content-type": "application/x-www-form-urlencoded",
+    Accept: "application/json",
+  },
 });
 
 export default api;
