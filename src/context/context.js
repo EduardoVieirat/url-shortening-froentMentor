97aa977c382b5cc76cc1233072070ebb97cf7019
EduardoOdev <duduvieiratou@outlook.com>
2024-09-07 18:23:13 -0300

    [feat]: confg the context

--- src/context/context.js
+++ src/context/context.js
@@ -3,16 +3,12 @@
 import UrlHooks from "@/hooks/UrlHooks";
 import { createContext } from "react";
 
-export const UrlContext = createContext([]);
+export const UrlContext = createContext({});
 
 export default function UrlProvider({ children }) {
-  const { urlToShort, setUrlToShort, urlShorted, setUrlShorted } = UrlHooks();
+  const useUrlProvider = UrlHooks();
 
   return (
-    <UrlContext.Provider
-      value={{ urlToShort, setUrlToShort, urlShorted, setUrlShorted }}
-    >
-      {children}
-    </UrlContext.Provider>
+    <UrlContext.Provider value={useUrlProvider}>{children}</UrlContext.Provider>
   );
 }
