4d4ec4a97a4b4963dd9e8c9f7074caca8cb44e1f
EduardoOdev <duduvieiratou@outlook.com>
2024-09-07 15:46:21 -0300

    [refact]: changing the states on urlhooks

--- src/hooks/UrlHooks.js
+++ src/hooks/UrlHooks.js
@@ -1,27 +1,22 @@
 "use client";
-
 import { useState } from "react";
 
 export default function UrlHooks() {
   const [urlToShort, setUrlToShort] = useState("");
 
   const [urlShorted, setUrlShorted] = useState({
-    code: "",
-    short_link: "",
-    full_short_link: "",
-    short_link2: "",
-    full_short_link2: "",
-    short_link3: "",
-    full_short_link3: "",
-    share_link: "",
-    full_share_link: "",
-    original_link: "",
+    shortUrl: "",
+    originalLink: "",
   });
+
+  const [errorMessage, setErrorMessage] = useState("");
 
   return {
     urlToShort,
     setUrlToShort,
     urlShorted,
     setUrlShorted,
+    errorMessage,
+    setErrorMessage,
   };
 }
